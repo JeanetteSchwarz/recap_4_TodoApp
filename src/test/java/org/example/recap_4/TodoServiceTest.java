aadb718e7633a55b69ecd1a2f4b66ea99ab6248f
JeanetteSchwarz <jeanette.schwarz@t-online.de>
2024-09-17 09:52:09 +0200

    ServiceClass Tests completed

--- src/test/java/org/example/recap_4/TodoServiceTest.java
+++ src/test/java/org/example/recap_4/TodoServiceTest.java
@@ -3,8 +3,10 @@ package org.example.recap_4;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,6 +27,56 @@ class TodoServiceTest {
         //THEN
         assertEquals(expected, actual);
     }
+
+    @Test
+    void test_getTodoByID() {
+        //GIVEN
+        Todo testTodo = new Todo("123", "learn java", Status.OPEN);
+        mockedRepo.save(testTodo);
+        Todo expected = testTodo;
+        //WHEN
+        when(mockedRepo.findById(testTodo.id())).thenReturn(Optional.of(expected));
+        Todo actual = testService.getTodoByID(testTodo.id());
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_addTodo(){
+        //GIVEN
+        Todo testTodo = new Todo("123", "learn java", Status.OPEN);
+        Todo expected = testTodo;
+        //WHEN
+        when(mockedRepo.save(any(Todo.class))).thenReturn(expected);
+        Todo actual = testService.addTodo(testTodo);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_updateTodo(){
+        //GIVEN
+        Todo testTodo = new Todo("123", "learn java", Status.OPEN);
+        Todo expected = new Todo("123", "learn java", Status.IN_PROGRESS);
+        mockedRepo.save(testTodo);
+        //WHEN
+        when(mockedRepo.save(any(Todo.class))).thenReturn(expected);
+        Todo actual = testService.updateTodo("123", expected);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_deleteTodo(){
+        //GIVEN
+        Todo testTodo = new Todo("123", "learn java", Status.OPEN);
+        mockedRepo.save(testTodo);
+        String expected = "Task deleted";
+        //WHEN
+        String actual = testService.deleteTodo(testTodo.id());
+        //THEN
+        assertEquals(expected, actual);
+        }
 
 
 }
